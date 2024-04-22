807aafd7aac7b990eaa0046bb491bd6214df6cd0
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-04-22 15:35:33 +0200

    feat(api): add database endpoints

---
+++ src/runtime/server/api/notion/databases/[id].get.ts
@@ -0,0 +1,27 @@
+import { defineEventHandler, getRouterParams } from 'h3'
+import { isNotionClientError } from '@notionhq/client'
+import { getNotionClient, handleNotionError } from '#server/utils'
+
+export default defineEventHandler(async (event) => {
+  if (!event.context.params) return { statusCode: 400, message: 'Bad Request' }
+
+  const { id } = getRouterParams(event)
+
+  try {
+    const client = getNotionClient()
+    const response = await client.databases.retrieve({ database_id: id })
+
+    return response
+  }
+  catch (error) {
+    if (isNotionClientError(error)) {
+      handleNotionError(error)
+    }
+    else {
+      return {
+        statusCode: 500,
+        message: 'Internal Server Error',
+      }
+    }
+  }
+})
