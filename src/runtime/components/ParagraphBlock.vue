0b873467566249fd7063c141337850a7f166fd11
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-04-30 19:52:34 +0200

    feat(render): add Paragraph component

---
+++ src/runtime/components/ParagraphBlock.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import type { ParagraphBlockObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+import RichText from './utils/RichText.vue'
+
+const props = defineProps<{
+  block: ParagraphBlockObjectResponse
+}>()
+
+const richText = computed(() => {
+  return props.block.paragraph.rich_text
+})
+</script>
+
+<template>
+  <div>
+    <RichText
+      v-for="text in richText"
+      :key="text.plain_text"
+      :rich-text="text"
+    />
+  </div>
+</template>
