c9e067597f4d75bae67a72ba54646ea53d539846
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-05-01 15:31:43 +0200

    feat(render): add QuoteBlock component

---
+++ src/runtime/components/QuoteBlock.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import type { QuoteBlockObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+import { computed } from 'vue'
+import RichText from './utils/RichText.vue'
+
+const props = defineProps<{
+  block: QuoteBlockObjectResponse
+}>()
+
+const quote = computed(() => {
+  return props.block.quote.rich_text
+})
+</script>
+
+<template>
+  <div class="blockquote">
+    <div style="background-color: black;" />
+    <RichText
+      v-for="text in quote"
+      :key="text.plain_text"
+      :rich-text="text"
+    />
+  </div>
+</template>
+
+<style scoped>
+.blockquote {
+  position: relative;
+  padding: 1rem;
+  border-left: 4px solid #333;
+}
+</style>
